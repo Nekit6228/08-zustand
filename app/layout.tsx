436452d470a5c759378160c0e89e160337b775fa
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-07-04 12:54:09 +0200

    metadata layout proverka

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,27 @@ import './globals.css';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import { Metadata } from 'next';
 
-export const metadata = {
-  title: 'NoteHub',
-  description: 'Personal note management app',
-};
+
+export const  metadata :Metadata ={
+  title:'Notes',
+  description:'Main Notes Page and Creating New Notes',
+  icons:'/notehub-og-meta.jpg',
+  openGraph:{
+    title:'Notes',
+    description:'Main Notes Page and Creating New Notes',
+    url:'https://08-zustand-black.vercel.app/',
+    siteName:'Notes',
+    images:[{
+      url:'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+      width:1200,
+      height:630,
+      alt:'Main Notes'
+    }]
+  }
+}
+
 
 export default function RootLayout({
   children,
